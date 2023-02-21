186f299972b238f52266f721b21c8d64c612a8d2
Rino Setiyo <42023075+rinosetiyo@users.noreply.github.com>
2023-02-21 06:18:54 +0000

    modify subscribe model and migration

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-
 # Create your models here.
+NEWSLETTER_OPTION = [('w', 'Weekly'), ('M', 'Monthly')]
+
+
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    option = models.CharField(max_length=2, choices=NEWSLETTER_OPTION)
 
-    def __str__(self):
-        return self.first_name
+    # def __str__(self):
+    #     return self.first_name
