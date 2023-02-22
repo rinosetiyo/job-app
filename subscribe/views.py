723c158428c7ab365593064b51a82f56ecf845e0
Rino Setiyo <42023075+rinosetiyo@users.noreply.github.com>
2023-02-22 04:39:58 +0000

    change views.py subscribe

--- subscribe/views.py
+++ subscribe/views.py
@@ -3,24 +3,27 @@ from django.urls import reverse
 from subscribe.forms import SubscribeForm
 from subscribe.models import Subscribe
 
+
 # Create your views here.
 def subscribe(request):
-  subscribe_form = SubscribeForm()
-  if request.POST:
-    subscribe_form = SubscribeForm(request.POST)
-    if subscribe_form.is_valid():
-      subscribe_form.save()
-      # print("valid form")
-      # first_name = subscribe_form.cleaned_data['first_name']
-      # last_name = subscribe_form.cleaned_data['last_name']
-      # email = subscribe_form.cleaned_data['email']
-      # subscribe = Subscribe(first_name = first_name, last_name = last_name, email = email)
-      # subscribe.save()
-      return redirect(reverse("thank_you"))
-      
-  context = {"form": subscribe_form,}
-  return render(request, "subscribe/subscribe.html", context)
+    subscribe_form = SubscribeForm()
+    if request.POST:
+        subscribe_form = SubscribeForm(request.POST)
+        if subscribe_form.is_valid():
+            subscribe_form.save()
+            # first_name = subscribe_form.cleaned_data['first_name']
+            # last_name = subscribe_form.cleaned_data['last_name']
+            # email = subscribe_form.cleaned_data['email']
+            # subscribe = Subscribe(first_name = first_name, last_name = last_name, email = email)
+            # subscribe.save()
+            return redirect(reverse("thank_you"))
+
+    context = {
+        "form": subscribe_form,
+    }
+    return render(request, "subscribe/subscribe.html", context)
+
 
 def thank_you(request):
-  context = {}
-  return render(request, "subscribe/thank_you.html", context)
+    context = {}
+    return render(request, "subscribe/thank_you.html", context)
